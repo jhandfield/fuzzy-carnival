9cd5cbf65b6e81dac63b98c5c32d2fc1a70a0034
Justin Handfield <justin@handfield.org>
2018-01-19 21:35:21 -0500

    Moved Hue Bridge settings to config.json, added connectivity test to the Bridge during setup, regorganized some of the application code.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const events = require("events");
 const eventEmitter = new events.EventEmitter();
 const http = require("http");
+const rp = require("request-promise");
 
 // Third-party libraries
 const dateFormat = require("dateformat");
@@ -30,9 +31,6 @@ const logger = new winston.Logger({
 	]
 });
 
-const hueIP = "192.168.1.49";		// IP address of the Hue bridge
-const hueUsername = "PlX16CEbg1u5iyZuxc-kie1cMPDbGn0-6zaKBGge";		// Username to use with the Hue API
-
 // Configure the logger as per settings in the app configuration
 if ((config.hasOwnProperty("logging") && config.logging.hasOwnProperty("enabled") && !config.logging.enabled) || !config.hasOwnProperty("logging"))
 {
@@ -48,7 +46,7 @@ else {
 		// Override the default console level if specified
 		if (config.hasOwnProperty("logging") && config.logging.hasOwnProperty("consoleLoggingLevel"))
 		{
-			logger.info(`Setting console logging level to ${config.logging.consoleLoggingLevel} as per configuration`);
+			logger.debug(`Setting console logging level to ${config.logging.consoleLoggingLevel} as per configuration`);
 
 			// Just remove the console logger and readd it, seems simplest
 			logger.remove(winston.transports.Console)
@@ -93,63 +91,105 @@ else {
 			}
 		});
 	}
+	
+	// Config must specify bridge.address
+	if (!config.hasOwnProperty("bridge") || (config.hasOwnProperty("bridge") && (!config.bridge.hasOwnProperty("address") || (config.bridge.hasOwnProperty("address") && config.bridge.address.length < 1))))
+	{
+		logger.error("Your Hue Bridge's address must be specified in the configuration file as bridge.address; exiting.");
+		process.exit(1);
+	}
+	
+	// Config must specify bridge.Username
+	if (!config.hasOwnProperty("bridge") || (config.hasOwnProperty("bridge") && (!config.bridge.hasOwnProperty("username") || (config.bridge.hasOwnProperty("username") && config.bridge.username.length < 1))))
+	{
+		logger.error("Your Hue Bridge's username must be specified in the configuration file as bridge.username; exiting.");
+		process.exit(1);
+	}
 
-	logger.info(`Application finished initializing at ${dateFormat(Date.now(), "d mmm yyyy HH:MM:ss")}, beginning normal operation.`);
+    // Report bridge address and username
+    logger.info(`Hue Bridge specified at address ${config.bridge.address} with username ${config.bridge.username}`);
+    
+    // Test connectivity with the bridge
+    rp.get({ uri: `http://${config.bridge.address}/api/${config.bridge.username}` })
+        .then((res) => {
+            // Parse the response
+            let parsedResponse = JSON.parse(res);
+
+            // Check for an error response in the body
+            if (Array.isArray(parsedResponse) && parsedResponse[0].hasOwnProperty("error")) {
+                logger.error(`Connection test to Hue Bridge failed - received error response: ${parsedResponse[0].error.description}`);
+                process.exit(0);
+            }
+            // Connection test succeeded, continue setting up
+            else {
+                logger.info("Connection test to Hue Bridge succeeded.");
+                logger.info(`Application finished initializing at ${dateFormat(Date.now(), "d mmm yyyy HH:MM:ss")}, beginning normal operation.`);
+
+                // Finish configuring the app
+                configureExpress();
+            }
+        })
+        // Catch errors making the request (timeout, bad address, etc.)
+        .catch((err) => {
+            logger.silly(`Hue connection test error output: ${err}`);
+            logger.error("Connection test to Hue Bridge failed - check your settings.");
+            process.exit(1);
+        });
 }
 
-// Basic test route, verifies that the application is working
-app.get('/', function (req, res) {
-	res.send('Hello World!')
-})
+function configureExpress() {
+    // Basic test route, verifies that the application is working
+    app.get('/', function (req, res) {
+        res.send('Hello World!')
+    })
 
-// Retrieve current status of users
-app.get('/users', (req, res) => {
-	res.send(config.users);
-});
+    // Retrieve current status of users
+    app.get('/users', (req, res) => {
+        res.send(config.users);
+    });
 
-// Update a user's state
-app.put('/user/:userId/:state', function (req, res) {
-	// Test that we recognize the userId
-	if (config.users.find(u => u.id === req.params.userId) == null)
-		{
-			logger.error(`No user with ID ${req.params.userId} exists.`);
+    // Update a user's state
+    app.put('/user/:userId/:state', function (req, res) {
+        // Test that we recognize the userId
+        if (config.users.find(u => u.id === req.params.userId) == null) {
+            logger.error(`No user with ID ${req.params.userId} exists.`);
 
-			res.status(400);
-			res.send(`No user exists with ID ${req.params.userId}`);
-			return;
-		}
-	
-	// Test that the state provided is valid
-	if (req.params.state !== "home" && req.params.state !== "away")
-		{
-			logger.error(`Invalid state ${req.params.state} provided`);
+            res.status(400);
+            res.send(`No user exists with ID ${req.params.userId}`);
+            return;
+        }
 
-			res.status(400);
-			res.send(`Invalid user state "${req.params.state}" provided - state must be either "home" or "away"`);
-			return;
-		}
+        // Test that the state provided is valid
+        if (req.params.state !== "home" && req.params.state !== "away") {
+            logger.error(`Invalid state ${req.params.state} provided`);
 
-	// All is well - log the request and update the state
-	const user = config.users.find(u => u.id === req.params.userId);
-	const origState = user.state;
-	const newState = req.params.state;
+            res.status(400);
+            res.send(`Invalid user state "${req.params.state}" provided - state must be either "home" or "away"`);
+            return;
+        }
 
-	logger.info(`Changing state of user ${req.params.userId} (${user.name}) from ${user.state} to ${req.params.state}`);
+        // All is well - log the request and update the state
+        const user = config.users.find(u => u.id === req.params.userId);
+        const origState = user.state;
+        const newState = req.params.state;
 
-	// Update the user
-	user.state = req.params.state;
+        logger.info(`Changing state of user ${req.params.userId} (${user.name}) from ${user.state} to ${req.params.state}`);
 
-	// Emit a userStateChanged event
-	eventEmitter.emit("userStateChanged", user, origState, newState);
+        // Update the user
+        user.state = req.params.state;
 
-	// Send a response
-	res.send("OK");
-});
+        // Emit a userStateChanged event
+        eventEmitter.emit("userStateChanged", user, origState, newState);
 
-// Start Express
-app.listen(config.application.port, function () {
-	logger.info(`Express listening for connections on port ${config.application.port}`);
-})
+        // Send a response
+        res.send("OK");
+    });
+
+    // Start Express
+    app.listen(config.application.port, function () {
+        logger.info(`Express listening for connections on port ${config.application.port}`);
+    })
+}
 
 eventEmitter.on("userStateChanged", (user, oldState, newState) => {
 	logger.debug(`userStateChanged event fired with params: user = ${JSON.stringify(user)}, oldState = ${oldState}, newState = ${newState}`);
@@ -192,7 +232,7 @@ function changeLightState(lightID, state)
 {
 	logger.debug(`Changing state of light ${lightID} to ${state}`);
 
-	const path = `/api/${hueUsername}/lights/${lightID}/state`;
+	const path = `/api/${config.hue.bridgeUsername}/lights/${lightID}/state`;
 	const body = `{"on":${state}}`;
 
 	makeHueRequest(path, body);
@@ -202,7 +242,7 @@ function makeHueRequest(path, body) {
 	logger.silly(`Making request to hue at path ${path} with body ${body}`);
 
 	const options = {
-		hostname: hueIP,
+		hostname: config.hue.bridgeIP,
 		port: 80,
 		path: path,
 		method: "PUT"
